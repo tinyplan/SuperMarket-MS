7b411ac0c7910098443fa24c1734e19500430de9
tinyPlan <3405443386@qq.com>
2021-05-27 22:36:28 +0800

    init:初始化仓库

---
+++ src/main/resources/sql/supermarket_ms.sql
@@ -0,0 +1,88 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80017
+ Source Host           : localhost:3306
+ Source Schema         : supermarket_ms
+
+ Target Server Type    : MySQL
+ Target Server Version : 80017
+ File Encoding         : 65001
+
+ Date: 27/05/2021 22:29:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods_base_info
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_base_info`;
+CREATE TABLE `goods_base_info`  (
+  `id` int(11) NOT NULL,
+  `goods_base_id` int(255) NOT NULL,
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `goods_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for goods_info
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_info`;
+CREATE TABLE `goods_info`  (
+  `id` int(11) NOT NULL,
+  `goods_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `goods_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `goods_cost` decimal(10, 2) NOT NULL,
+  `goods_price` decimal(10, 2) NOT NULL,
+  `production_date` datetime(0) NULL,
+  `duration` int(255) NOT NULL COMMENT '保质期',
+  `expiration_date` datetime(0) NOT NULL COMMENT '过期时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for import
+-- ----------------------------
+DROP TABLE IF EXISTS `import`;
+CREATE TABLE `import`  (
+  `id` int(11) NOT NULL,
+  `import_id` int(255) NOT NULL,
+  `goods_base_id` int(255) NOT NULL COMMENT '商品基础编号',
+  `import_date` datetime(0) NOT NULL COMMENT '进货日期',
+  `import_goods_sum` int(255) NOT NULL COMMENT '进货数量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`  (
+  `id` int(11) NOT NULL,
+  `order_id` int(255) NOT NULL,
+  `total_cost` decimal(10, 2) NOT NULL,
+  `buy_time` datetime(0) NOT NULL COMMENT '购买时间',
+  `invalid_time` datetime(0) NOT NULL COMMENT '无法退货时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for order_item
+-- ----------------------------
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE `order_item`  (
+  `id` int(11) NOT NULL,
+  `order_id` int(255) NOT NULL,
+  `goods_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `goods_sum` int(255) NOT NULL,
+  `goods_price` decimal(10, 2) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
