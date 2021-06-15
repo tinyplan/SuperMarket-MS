689859be26099372d38ef3fd6f050b2eb648e81f
SouthYou <405771331@qq.com>
2021-06-15 16:57:24 +0800

    fix:修复了退款功能的bug

--- src/main/java/com/software/demo/entity/dto/RefundGoodsDTO.java
+++ src/main/java/com/software/demo/entity/dto/RefundGoodsDTO.java
@@ -1,12 +1,20 @@
 package com.software.demo.entity.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author yjn
  * 2021/06/14
  */
 public class RefundGoodsDTO {
+    @NotBlank(message = "记录编号不能为空")
     private String recordId;
+    @NotBlank(message = "商品编号不能为空")
     private String goodsId;
+    @Min(value = 0, message = "退款数量不能为负数")
+    @Max(value = 10000, message = "退款数量需小于10000")
     private Integer goodsNum;
 
     public RefundGoodsDTO() {
